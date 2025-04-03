2f127ae28dcccc235838f426ba19c4aed702f815
Moeus <2662717402@qq.com>
2025-04-03 23:14:05 +0800

    测试

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import { useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Square(props) {
+  // 解包props
+  const { index, updateSquares, step, updaestep } = props;
+  let [value, setValue] = useState(null);
+
+  function handleClick() {
+    // 检测是否已经点击
+    if (value!== null) {
+      return;
+    }
+    if (step %2== 1) {
+      setValue('X');
+      // 更新父组件的状态
+      updaestep(step + 1);
+      updateSquares(index,"X");
+    }else {
+      setValue('O');
+      // 更新父组件的状态
+      updaestep(step + 1);
+      updateSquares(index,"O");
+    }
+    console.log({props});
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {/*   //xml内对js变量的引用需要使用花括号{} */}
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const[step,setStep] =useState(1);
+  // 形成闭包，函数handleClick无论在哪调用都可以访问到squares
+  function handleClick(i,val) {
+    const squaresCopy = squares.slice();
+    squaresCopy[i-1] =val;
+    setSquares(squaresCopy);
+    console.log(squaresCopy);
+  }
+  return (
+    <>
+      <h1 class="items-center">Welcome to Tic Tac Toe</h1>
+      <div className="board-row">
+        <Square index={1} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={2} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={3} updateSquares={handleClick} step={step} updaestep={setStep} />
+      </div>
+      <div className="board-row">
+        <Square index={4} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={5} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={6} updateSquares={handleClick} step={step} updaestep={setStep} />
+      </div>
+      <div className="board-row">
+        <Square index={7} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={8} updateSquares={handleClick} step={step} updaestep={setStep} />
+        <Square index={9} updateSquares={handleClick} step={step} updaestep={setStep} />
+      </div>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
   </StrictMode>,
 )
